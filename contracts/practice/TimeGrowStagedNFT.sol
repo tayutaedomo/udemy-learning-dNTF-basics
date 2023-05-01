f1c9186c0730c96740ea94b887d0497e932423a3
tayutaedomo <tayutaedomo@gmail.com>
2023-05-02 01:29:15 +0900

    TimeGrowStagedNFT.sol を追加

--- contracts/practice/TimeGrowStagedNFT.sol
+++ contracts/practice/TimeGrowStagedNFT.sol
@@ -1,1 +1,24 @@
 // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts@4.8.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.8.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
+
+/// @title 時間で成長する NFT
+/// @dev time-based を使う
+contract TimeGrowStagedNFT is ERC721, ERC721URIStorage, Ownable {
+
+    constructor() ERC721("TimeGrowStagedNFT", "TGS") {}
+
+    /// @dev 以下は全ての override 重複の整理
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
