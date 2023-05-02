1dfa8b50e045c82e3e692cbb9ae3c2d66348c99a
tayutaedomo <tayutaedomo@gmail.com>
2023-05-02 16:15:36 +0900

    TimeGrowStagedNFT: 状態変数などを追加

--- contracts/practice/TimeGrowStagedNFT.sol
+++ contracts/practice/TimeGrowStagedNFT.sol
@@ -10,8 +10,33 @@ import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
 /// @title 時間で成長する NFT
 /// @dev time-based を使う
 contract TimeGrowStagedNFT is ERC721, ERC721URIStorage, Ownable {
+    /// @dev Counters ライブラリの全 Function を構造体 Counter 型に付与
+    using Counters for Counters.Counter;
+
+    /// @dev 付与した Counter 型の変数を定義
+    Counters.Counter private _tokenIdCounter;
+
+    /// @dev stage 設定
+    enum Stages { Baby, Child, Youth, Adult, Granpa }
+
+    /// @dev mint 時に設定する成長ステップを定数化
+    Stages public constant firstStage = Stages.Baby;
+
+    /// @dev tokenId と現ステージをマッピングする変数を定義
+    mapping(uint => Stages) public tokenStage;
+
+    /// @dev NFT mint 時は特定の URI を指定する
+    string public startFile = "metadata1.json";
+
+    /// @dev URI 更新時に記録する
+    event UpdateTokenURI(address indexed sender, uint256 indexed tokenId, string uri);
 
     constructor() ERC721("TimeGrowStagedNFT", "TGS") {}
+
+    /// @dev metadata 用の baseURI を設定する
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://bafybeichxrebqguwjqfyqurnwg5q7iarzi53p64gda74tgpg2uridnafva/";
+    }
 
     /// @dev 以下は全ての override 重複の整理
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
